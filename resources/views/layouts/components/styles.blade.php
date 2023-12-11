33e90fb4214680e4516c7425076d0655bfaaabcb
abd-elqader <abdelqader.montaser@gmail.com>
2023-12-11 19:44:29 +0200

    collection

---
+++ resources/views/layouts/components/styles.blade.php
@@ -0,0 +1,6 @@
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
